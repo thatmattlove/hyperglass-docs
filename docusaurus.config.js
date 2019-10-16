e7bdf577145ff7de6153c23c06c35831d8443e6a
checktheroads <matt@allroads.io>
2019-10-16 01:17:14 -0700

    add devices

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,77 +6,75 @@
  */
 
 module.exports = {
-  title: 'hyperglass',
-  tagline: 'A modern, customizable network looking glass written in Python 3',
-  url: 'https://hyperglass.io',
-  baseUrl: '/',
-  favicon: 'img/hyperglass.ico',
-  organizationName: 'checktheroads', // Usually your GitHub org/user name.
-  projectName: 'hyperglass', // Usually your repo name.
+  title: "hyperglass",
+  tagline: "A modern, customizable network looking glass written in Python 3",
+  url: "https://hyperglass.io",
+  baseUrl: "/",
+  favicon: "img/hyperglass.ico",
+  organizationName: "checktheroads", // Usually your GitHub org/user name.
+  projectName: "hyperglass", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'hyperglass',
-      logo: {
-        src: '',
-      },
+      title: "hyperglass",
+      // logo: {alt: '', src: ''},
       links: [
-        { to: 'docs/installation', label: 'Docs', position: 'left' },
+        { to: "docs/installation", label: "Docs", position: "left" },
         {
-          href: 'https://github.com/checktheroads/hyperglass',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
+          href: "https://github.com/checktheroads/hyperglass",
+          label: "GitHub",
+          position: "right"
+        }
+      ]
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Docs',
-              to: 'docs/doc1',
-            },
-          ],
+              label: "Docs",
+              to: "docs/doc1"
+            }
+          ]
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-          ],
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus"
+            }
+          ]
         },
         {
-          title: 'Social',
+          title: "Social",
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
-            },
-          ],
-        },
+              label: "Blog",
+              to: "blog"
+            }
+          ]
+        }
       ],
       logo: {
-        alt: 'Facebook Open Source Logo',
-        src: 'https://docusaurus.io/img/oss_logo.png',
+        alt: "Facebook Open Source Logo",
+        src: "https://docusaurus.io/img/oss_logo.png"
       },
-      copyright: `Copyright © ${new Date().getFullYear()} Matt Love`,
-    },
+      copyright: `Copyright © ${new Date().getFullYear()} Matt Love`
+    }
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js")
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
-    ],
-  ],
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      }
+    ]
+  ]
 };
